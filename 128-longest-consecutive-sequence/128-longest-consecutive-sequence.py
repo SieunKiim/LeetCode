843388e5edba630b29792ddd5e93e8f297f11a3c
sieun <sieun960513@naver.com>
2022-07-05 18:14:20 +0900

    Time: 406 ms (79.20%), Space: 31.5 MB (13.94%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,24 +1,38 @@
 import heapq
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        nums.sort()
+        dic = dict()
+        for n in nums:
+            dic[n] = 1
         Max = 0
-        count = 1
-        last = nums[0]
-        for i in range(1,len(nums)):
-            if last +1 ==  nums[i]:
-                count += 1
-            elif last == nums[i]:
-                continue
-            else:
-                Max = max(Max, count)
-                count = 1
-            last = nums[i]
-        Max = max(Max, count)
+        for k in dic:
+            temp = k - 1
+            while dic.get(temp):
+                dic[k] += dic[temp]
+                dic[temp] = 0
+                temp -= 1
+            Max = max(Max, dic[k])
         return Max
-            
+        
+#----------------------------------------------------------------------------
+        # if len(nums) == 0:
+        #     return 0
+        # nums.sort()
+        # Max = 0
+        # count = 1
+        # last = nums[0]
+        # for i in range(1,len(nums)):
+        #     if last +1 ==  nums[i]:
+        #         count += 1
+        #     elif last == nums[i]:
+        #         continue
+        #     else:
+        #         Max = max(Max, count)
+        #         count = 1
+        #     last = nums[i]
+        # Max = max(Max, count)
+        # return Max
+#-------------------------------------------------------------------------------
 #         heapq.heapify(nums)
         
 #         last = heapq.heappop(nums)
@@ -35,4 +49,4 @@ class Solution:
 #                 count = 1
 #             last = temp
 #         Max = max(Max, count)
-        return Max
+#        return Max
