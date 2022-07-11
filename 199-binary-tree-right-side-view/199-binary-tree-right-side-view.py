ce73eca1fb1056aeac434520e7d623b1c179001e
sieun <sieun960513@naver.com>
2022-07-11 10:44:23 +0900

    Time: 33 ms (93.50%), Space: 13.9 MB (22.93%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -1,37 +1,51 @@
+# import sys
+# sys.setrecursionlimit(10**7)
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 from collections import deque
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        i = 0
-        
-        result = []      
-        
-        if root == None:
-            return []
-        else:
-            result.append(root.val)
-        que = deque()
-        que.append(root)
-        while True:
-            temp = []
-            length = len(que)
-            for i in range(length):
-                q = que.popleft()
-                
-                if q.left:
-                    que.append(q.left)
-                    temp.append(q.left.val)
-                if q.right:
-                    que.append(q.right)
-                    temp.append(q.right.val)
+        result = []
+        def dfs(tree, deep):
+            if len(result) == deep:
+                result.append(tree.val)
             
-            if len(temp):
-                result.append(temp[-1])
-            else:
-                break
-        return (result)
+            if tree.right:
+                dfs(tree.right, deep+1)
+            if tree.left:
+                dfs(tree.left, deep+1)
+        
+        if root:
+            dfs(root, 0)
+        return result
+                
+#         deque 로 풀었음 ---------------------
+#         result = []
+#         if root == None:
+#             return []
+#         else:
+#             result.append(root.val)
+#         que = deque()
+#         que.append(root)
+#         while True:
+#             temp = []
+#             length = len(que)
+#             for i in range(length):
+#                 q = que.popleft()
+                
+#                 if q.left:
+#                     que.append(q.left)
+#                     temp.append(q.left.val)
+#                 if q.right:
+#                     que.append(q.right)
+#                     temp.append(q.right.val)
+            
+#             if len(temp):
+#                 result.append(temp[-1])
+#             else:
+#                 break
+#         return (result)
