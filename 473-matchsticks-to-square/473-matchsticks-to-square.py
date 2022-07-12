ef2915f4b38540aab43f34f2c81de879428aac5b
sieun <sieun960513@naver.com>
2022-07-12 11:12:26 +0900

    Time: 7100 ms (16.01%), Space: 13.8 MB (86.43%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -0,0 +1,40 @@
+class Solution:
+    
+        
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        result = False
+        length = sum(matchsticks)//4
+        # print("한 변의 길이",length)
+        if max(matchsticks) > length:
+            return False
+        count = 4
+        while max(matchsticks) == length:
+            matchsticks.remove(length)
+            count -= 1
+            if len(matchsticks) == 0:
+                return True
+        
+        if sum(matchsticks) == length*count:
+            matchsticks.sort()
+            used = [0] * len(matchsticks)
+            return self.dfs(0, matchsticks, used, count, length, 0)
+        else:
+            return False
+        
+    def dfs(self,temp , matchs, used, count, length, index):
+        if count == 0:
+            return True
+        res = False
+        for i in range(index, len(matchs)):
+            if used[i] == 0:
+                if temp + matchs[i] < length:
+                    used[i] = 1
+                    res = self.dfs(temp + matchs[i], matchs, used, count, length, i+1)
+
+                elif temp + matchs[i] == length:
+                    used[i] = 1
+                    res = self.dfs(0, matchs, used, count-1, length, 0) | res
+                if res:
+                    return res
+                used[i] = 0
+        return res
