1b15cc46811f9fd18da6d0c59fb636c29433fabc
sieun <sieun960513@naver.com>
2022-08-07 14:06:14 +0900

    Time: 742 ms (44.56%), Space: 18.4 MB (52.30%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,6 +1,6 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        print("")
+        mod = 10**9 + 7
         dp = [[0 for _ in range(5)] for _ in range(n+1)]
         for i in range(5):
             dp[1][i] = 1
@@ -11,16 +11,16 @@ class Solution:
         for i in range(2, n+1):
             for j in range(5):
                 if j == 0:
-                    dp[i][j] = dp[i-1][1] + dp[i-1][2] + dp[i-1][4]
+                    dp[i][j] = (dp[i-1][1] + dp[i-1][2] + dp[i-1][4])%mod
                 elif j == 1:
-                    dp[i][j] = dp[i-1][0] + dp[i-1][2]
+                    dp[i][j] = (dp[i-1][0] + dp[i-1][2])%mod
                 elif j == 2:
-                    dp[i][j] = dp[i-1][1] + dp[i-1][3]
+                    dp[i][j] = (dp[i-1][1] + dp[i-1][3])%mod
                 elif j == 3:
-                    dp[i][j] = dp[i-1][2]
+                    dp[i][j] = (dp[i-1][2])%mod
                 else:
-                    dp[i][j] = dp[i-1][3] + dp[i-1][2]
+                    dp[i][j] = (dp[i-1][3] + dp[i-1][2])%mod
         
         # for i in range(n+1):
         #     print(dp[i])
-        return (sum(dp[i]) % (10**9 + 7))
+        return (sum(dp[i]) % (mod))
