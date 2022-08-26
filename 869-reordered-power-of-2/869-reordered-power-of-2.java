d183fcfbc4f11f48420b298a265c1c0ad68ada9c
sieun <sieun960513@naver.com>
2022-08-26 10:55:10 +0900

    Time: 1 ms (100.00%), Space: 40.3 MB (84.67%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,29 +1,45 @@
 class Solution {
     public boolean reorderedPowerOf2(int n) {
-        int preset = 1;
-        HashMap<Integer, ArrayList<HashMap<Integer,Integer>>> dict = new HashMap<>();
-        while (preset <= Math.pow(10,9)){
-            int key = String.valueOf(preset).length();
-            ArrayList temp = dict.getOrDefault(key, new ArrayList<HashMap<Integer,Integer>>());
-            String num = String.valueOf(preset);
-            HashMap<Character, Integer> T = new HashMap<>();
-            for (int i = 0; i < num.length(); i++){
-                T.put(num.charAt(i), T.getOrDefault(num.charAt(i), 0)+1);
-            }
-            temp.add(T);
-            dict.put(key,temp);
-            preset *= 2;
-        }
-        String target = String.valueOf(n);
-        int wantLen  = String.valueOf(n).length();
-        HashMap<Character, Integer> wantDict = new HashMap<>();
-        for (int i = 0; i < wantLen; i++){
-            wantDict.put(target.charAt(i), wantDict.getOrDefault(target.charAt(i), 0) + 1);
-        }
-        System.out.println(wantDict);
-        if (dict.get(wantLen).contains(wantDict)){
-            return true;
-        }
+        int[] A = count(n);
+        for (int i = 0; i < 31; ++i)
+            if (Arrays.equals(A, count(1 << i)))
+                return true;
         return false;
     }
+
+    
+    public int[] count(int N) {
+        int[] ans = new int[10];
+        while (N > 0) {
+            ans[N % 10]++;
+            N /= 10;
+        }
+        return ans;
+    }
+        // int preset = 1;
+        // HashMap<Integer, ArrayList<HashMap<Integer,Integer>>> dict = new HashMap<>();
+        // while (preset <= Math.pow(10,9)){
+        //     int key = String.valueOf(preset).length();
+        //     ArrayList temp = dict.getOrDefault(key, new ArrayList<HashMap<Integer,Integer>>());
+        //     String num = String.valueOf(preset);
+        //     HashMap<Character, Integer> T = new HashMap<>();
+        //     for (int i = 0; i < num.length(); i++){
+        //         T.put(num.charAt(i), T.getOrDefault(num.charAt(i), 0)+1);
+        //     }
+        //     temp.add(T);
+        //     dict.put(key,temp);
+        //     preset *= 2;
+        // }
+        // String target = String.valueOf(n);
+        // int wantLen  = String.valueOf(n).length();
+        // HashMap<Character, Integer> wantDict = new HashMap<>();
+        // for (int i = 0; i < wantLen; i++){
+        //     wantDict.put(target.charAt(i), wantDict.getOrDefault(target.charAt(i), 0) + 1);
+        // }
+        // System.out.println(wantDict);
+        // if (dict.get(wantLen).contains(wantDict)){
+        //     return true;
+        // }
+        // return false;
+    // }
 }
