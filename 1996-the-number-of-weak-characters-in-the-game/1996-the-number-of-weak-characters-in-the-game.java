ad73ac1c9935747f904efe204e671f2a21deb084
sieun <sieun960513@naver.com>
2022-09-09 16:10:28 +0900

    Time: 922 ms (5.06%), Space: 91 MB (85.53%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -17,25 +17,21 @@ class Solution {
 //     }
     
     public int numberOfWeakCharacters(int[][] properties) {
-        // if we sort only by 1st indexes, there could be values where 1st index is the same 
-        // Arrays.sort(properties, (a,b) -> Integer.compare(a[0], b[0]));
-        
-        // b[1], a[1] because we need the 2nd values in descending order
-        Arrays.sort(properties, (a,b) -> (a[0] == b[0]) ? Integer.compare(b[1], a[1]) : Integer.compare(a[0], b[0]));
-        
-        int noOfWeakCharacters = 0;
-        int len = properties.length;
-       // improvization here - // we need to keep track of the max value
-        int max = properties[len-1][1];
-        
-        for(int i = len-2; i>=0 ;i--) {
-            
-            if(properties[i][1] < max) {
-                noOfWeakCharacters++;
+        Arrays.sort(properties, (a, b) -> (a[0] == b[0]) ? b[1] - a[1] : a[0] - b[0]);
+        for (int i = 0; i < properties.length; i++) {
+            System.out.println(Arrays.toString(properties[i]));
+        }
+        int count = 0;
+        int n = properties.length;
+//        int[] last = properties[0];
+        int last = properties[n-1][1];
+        for (int i = n-2; i >=0; i--) {
+            if (properties[i][1] < last) {
+                count+= 1;
             } else {
-                max = properties[i][1];
+                last = properties[i][1];
             }
         }
-        return noOfWeakCharacters;
+        return count;
     }
 }
