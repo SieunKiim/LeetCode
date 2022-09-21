895c6d1e5a72596055ea0f7cf80cc1289726018f
sieun <sieun960513@naver.com>
2022-09-21 09:59:56 +0900

    Time: 7 ms (60.77%), Space: 73.1 MB (35.69%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -2,17 +2,12 @@ class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int[] result = new int[queries.length];
         int index = 0;
-        // HashSet<Integer> evenIndex = new HashSet<>();
         int evenSum = 0;
-        // int tempIndex =0;
         for(int n : nums){
             if(n%2 == 0) {
-                // evenIndex.add(tempIndex);
                 evenSum += n;
             }
-            // tempIndex+=1;
         }
-        // System.out.println(evenIndex);
         for(int[] q : queries){
             int temp = nums[q[1]] + q[0];
             
@@ -20,16 +15,12 @@ class Solution {
                 if(nums[q[1]]%2 == 0) evenSum += q[0];
                 else {
                     evenSum += temp;
-                    // evenIndex.add(q[1]);
                 }
             }else{
                 if(nums[q[1]]%2 == 0) evenSum -= nums[q[1]];
-                // evenIndex.remove((Integer)q[1]);  
             }
-            
             result[index] = evenSum;
             nums[q[1]] = temp;
-            
             index +=1;
         }
         return result;
