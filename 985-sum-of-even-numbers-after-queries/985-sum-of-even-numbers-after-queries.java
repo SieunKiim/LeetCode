22920cb83484bf6b6805be1dcb3574be87b5d229
sieun <sieun960513@naver.com>
2022-09-21 09:59:25 +0900

    Time: 11 ms (19.61%), Space: 68 MB (84.24%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -2,29 +2,29 @@ class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int[] result = new int[queries.length];
         int index = 0;
-        HashSet<Integer> evenIndex = new HashSet<>();
+        // HashSet<Integer> evenIndex = new HashSet<>();
         int evenSum = 0;
-        int tempIndex =0;
+        // int tempIndex =0;
         for(int n : nums){
             if(n%2 == 0) {
-                evenIndex.add(tempIndex);
+                // evenIndex.add(tempIndex);
                 evenSum += n;
             }
-            tempIndex+=1;
+            // tempIndex+=1;
         }
         // System.out.println(evenIndex);
         for(int[] q : queries){
             int temp = nums[q[1]] + q[0];
             
             if(temp%2 == 0){
-                if(evenIndex.contains(q[1])) evenSum += q[0];
+                if(nums[q[1]]%2 == 0) evenSum += q[0];
                 else {
                     evenSum += temp;
-                    evenIndex.add(q[1]);
+                    // evenIndex.add(q[1]);
                 }
             }else{
-                if(evenIndex.contains(q[1])) evenSum -= nums[q[1]];
-                evenIndex.remove((Integer)q[1]);  
+                if(nums[q[1]]%2 == 0) evenSum -= nums[q[1]];
+                // evenIndex.remove((Integer)q[1]);  
             }
             
             result[index] = evenSum;
