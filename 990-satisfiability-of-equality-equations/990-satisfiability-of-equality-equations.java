540af8b3f961b0f1ff24a0f02aa08209f920e80b
sieun <sieun960513@naver.com>
2022-09-26 11:21:44 +0900

    Time: 45 ms (6.11%), Space: 44.4 MB (5.76%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -1,15 +1,11 @@
 class Solution {
     int[] alpha;
-    HashMap<Integer, HashSet<Integer>> ban;
     public boolean equationsPossible(String[] equations) {
         alpha = new int[26];
-        ban = new HashMap<>();
         for(int i = 0 ; i < 26; i++){
             alpha[i] = i;
         }
-        // System.out.println(Arrays.toString(alpha));
         for(String s : equations){
-            // System.out.println(Arrays.toString(alpha));
             boolean result = false;
             int index1 = s.charAt(0)-97;
             int index2 = s.charAt(3)-97;
@@ -39,23 +35,5 @@ class Solution {
         int py = getParent(y);
         if(px < py) alpha[py] = px;
         else alpha[px] =py;
-    }
-    
-    public void banned(int x, int y){
-        int px = getParent(x);
-        int py = getParent(y);
-        HashSet<Integer> temp1 = ban.getOrDefault(px, new HashSet<>());
-        temp1.add(py);
-        HashSet<Integer> temp2 = ban.getOrDefault(py, new HashSet<>());
-        temp2.add(px);
-        ban.put(px,temp1);
-        ban.put(py,temp2);   
-    }
-    public boolean isBanned(int x, int y){
-        int px = getParent(x);
-        int py = getParent(y);
-        if(ban.getOrDefault(px, new HashSet<>()).contains(py)) return true;
-        if(ban.getOrDefault(py, new HashSet<>()).contains(px)) return true;
-        return false;
     }
 }
