7a07f774908650ceca2e610f91fee0491f19bc4e
sieun <sieun960513@naver.com>
2022-09-30 21:58:21 +0900

    Time: 13 ms (91.77%), Space: 50.2 MB (84.58%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.java
+++ 218-the-skyline-problem/218-the-skyline-problem.java
@@ -1,34 +1,64 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<int[]> height = new ArrayList<>();
-        for(int[] b:buildings) {
-            height.add(new int[]{b[0], -b[2]});
-            height.add(new int[]{b[1], b[2]});
-        }
-        Collections.sort(height, (a, b) -> {
-                if(a[0] != b[0]) 
-                    return a[0] - b[0];
-                return a[1] - b[1];
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        int numOfBuildings = buildings.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                return b[2]-a[2];
+            }
         });
-        Queue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
-        pq.offer(0);
-        int prev = 0;
-        for(int[] h:height) {
-            if(h[1] < 0) {
-                pq.offer(-h[1]);
-            } else {
-                pq.remove(h[1]);
+        
+        int x = numOfBuildings==0?Integer.MAX_VALUE:buildings[0][0],next=0;
+        pq.offer(new int[]{Integer.MIN_VALUE,Integer.MAX_VALUE,0});
+        
+        while(x!=Integer.MAX_VALUE || next<numOfBuildings || pq.size()>1){
+            while(pq.size()>1 && pq.peek()[1]<=x){
+                pq.poll();
             }
-            int cur = pq.peek();
-            if(prev != cur) {
-                List<Integer>temp = new ArrayList<>();
-                temp.add(h[0]);
-                temp.add(cur);
-                result.add(temp);
-                prev = cur;
-            }
+            
+            while(next<numOfBuildings && x==buildings[next][0]) pq.offer(buildings[next++]);
+            
+            if(ans.size()==0 || pq.peek()[2]!=ans.get(ans.size()-1).get(1)) ans.add(
+                Arrays.asList(new Integer[]{x,pq.peek()[2]})
+            );
+            
+            // The next x position is either when the prev peak goes out of bound
+            // or the next building comes into picture(that may or may not change the skyline)
+            x = Math.min(pq.peek()[1],next>=numOfBuildings?Integer.MAX_VALUE:buildings[next][0]);
         }
-        return result;
+        
+        return ans;
+        
+        
+        // List<List<Integer>> result = new ArrayList<>();
+        // List<int[]> height = new ArrayList<>();
+        // for(int[] b:buildings) {
+        //     height.add(new int[]{b[0], -b[2]});
+        //     height.add(new int[]{b[1], b[2]});
+        // }
+        // Collections.sort(height, (a, b) -> {
+        //         if(a[0] != b[0]) 
+        //             return a[0] - b[0];
+        //         return a[1] - b[1];
+        // });
+        // Queue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+        // pq.offer(0);
+        // int prev = 0;
+        // for(int[] h:height) {
+        //     if(h[1] < 0) {
+        //         pq.offer(-h[1]);
+        //     } else {
+        //         pq.remove(h[1]);
+        //     }
+        //     int cur = pq.peek();
+        //     if(prev != cur) {
+        //         List<Integer>temp = new ArrayList<>();
+        //         temp.add(h[0]);
+        //         temp.add(cur);
+        //         result.add(temp);
+        //         prev = cur;
+        //     }
+        // }
+        // return result;
     }
 }
