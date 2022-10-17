7a8cba522b7c2e1789e52d39d7003f2e223194d2
sieun <sieun960513@naver.com>
2022-10-17 17:55:35 +0900

    Time: 6 ms (29.40%), Space: 42.1 MB (54.96%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean checkIfPangram(String s) {
         int n = s.length();
         int[] alpha = new int[26];
-        
         for (int i = 0; i< n ;i++){
             char now = s.charAt(i);
             if('a'<= now && now <= 'z'){
