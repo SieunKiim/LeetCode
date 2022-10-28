caa516daf200fe95998655ffd7b8e5c2b6e7e131
sieun <sieun960513@naver.com>
2022-10-28 10:02:23 +0900

    Time: 120 ms (6.63%), Space: 70.5 MB (7.08%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<HashMap<Character, Integer>, List<String>> dict = new HashMap<>();
+        for(String s : strs){
+            HashMap<Character, Integer> temp = new HashMap<>();
+            for(int i = 0; i< s.length(); i++){
+                temp.put(s.charAt(i), temp.getOrDefault(s.charAt(i), 0) + 1);
+            }
+            List<String> now = dict.getOrDefault(temp, new ArrayList<>());
+            now.add(s);
+            dict.put(temp, now);
+        }
+        return new ArrayList<>(dict.values());
+    }
+}
