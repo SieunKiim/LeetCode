9c8ca75265fcbaf79c3a43873336c8ada5ea2066
sieun <sieun960513@naver.com>
2022-10-28 10:09:34 +0900

    Time: 12 ms (82.36%), Space: 55.4 MB (70.87%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,15 +1,27 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<HashMap<Character, Integer>, List<String>> dict = new HashMap<>();
+        HashMap<String, List<String>> dict = new HashMap<>();
         for(String s : strs){
-            HashMap<Character, Integer> temp = new HashMap<>();
-            for(int i = 0; i< s.length(); i++){
-                temp.put(s.charAt(i), temp.getOrDefault(s.charAt(i), 0) + 1);
-            }
-            List<String> now = dict.getOrDefault(temp, new ArrayList<>());
+            char[] temp = s.toCharArray();
+            Arrays.sort(temp);
+            String target = new String(temp);
+            List<String> now = dict.getOrDefault(target, new ArrayList<>());
             now.add(s);
-            dict.put(temp, now);
+            dict.put(target, now);
         }
         return new ArrayList<>(dict.values());
+        
+        
+        // HashMap<HashMap<Character, Integer>, List<String>> dict = new HashMap<>();
+        // for(String s : strs){
+        //     HashMap<Character, Integer> temp = new HashMap<>();
+        //     for(int i = 0; i< s.length(); i++){
+        //         temp.put(s.charAt(i), temp.getOrDefault(s.charAt(i), 0) + 1);
+        //     }
+        //     List<String> now = dict.getOrDefault(temp, new ArrayList<>());
+        //     now.add(s);
+        //     dict.put(temp, now);
+        // }
+        // return new ArrayList<>(dict.values());
     }
 }
