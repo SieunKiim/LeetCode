8d155e5b5ff58c1487fc508f514495e7a2dbcba2
sieun <sieun960513@naver.com>
2022-10-31 09:54:55 +0900

    Time: 1 ms (93.83%), Space: 45.6 MB (62.59%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        if (n == 1) return true;
+        for(int i = 1; i < n ; i++){
+            for(int j = 1 ;j < m; j++){
+                if(matrix[i][j] != matrix[i-1][j-1]) return false;
+            }
+        }
+        return true;
+    }
+}
