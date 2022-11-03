ef26473a15257da2a9ae581ff71bf6b507890b18
sieun <sieun960513@naver.com>
2022-11-03 12:02:04 +0900

    Time: 198 ms (46.67%), Space: 124.9 MB (26.96%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -2,24 +2,20 @@ class Solution {
 
     public int longestPalindrome(String[] words) {
         int count = 0;
-        // int bigSameOdd = 0;
         HashMap<String, Integer> bag = new HashMap<>();
         HashMap<String, Integer> samesame = new HashMap<>();
         for(int i = 0; i < words.length;i++){
             if(words[i].charAt(0) == words[i].charAt(1)){
                 int temp = samesame.getOrDefault(words[i], 0);
-                // if((temp + 1)%2 == 1 )
-                //     bigSameOdd = Math.max(bigSameOdd, temp + 1);
                 samesame.put(words[i], temp+1);
             } else {
                 if(bag.containsKey(words[i])){
                     count += 1;
                     int temp = bag.get(words[i]);
-                    if (temp-1 == 0){
+                    if (temp-1 == 0)
                         bag.remove(words[i]);
-                    }else{
+                    else
                         bag.put(words[i], temp-1);
-                    }
                 } else {
                     StringBuffer sb = new StringBuffer(words[i]);
                     String target = sb.reverse().toString();
@@ -30,25 +26,18 @@ class Solution {
         int bigSame = 0;
         int bigOdd = 0;
         for(int val : samesame.values()){
-            if(val%2 == 0){
+            if(val%2 == 0)
                 bigSame += val;
-            } else {
+            else {
                 if(val> bigOdd){
                     bigSame += val;
-                    if(bigOdd != 0){
+                    if(bigOdd != 0)
                         bigSame -= 1;
-                    }
                     bigOdd= val;
-                } else{
-                    bigSame += (val-1);
-                }
-                
-            }
-            
+                } else
+                    bigSame += (val-1);   
+            }   
         }
-        // System.out.println(count +" || "+bigSame);
-        // System.out.println(samesame);
-        // System.out.println(bag);
         return count * 4 + bigSame * 2;
     }
 }
