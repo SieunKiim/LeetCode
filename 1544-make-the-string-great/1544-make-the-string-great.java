15ee91b901ac4f12b497285a1bd076f0d6f36e1a
sieun <sieun960513@naver.com>
2022-11-08 09:44:53 +0900

    Time: 6 ms (58.65%), Space: 43.4 MB (15.22%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -2,9 +2,6 @@ class Solution {
     public String makeGood(String s) {
         Stack<Character> stack = new Stack<>();
         for(int i =0 ;i<s.length(); i++){
-            // System.out.println(stack.toString());
-            // if(!stack.isEmpty())
-            //     System.out.println(stack.peek() - s.charAt(i));
             if(stack.empty() ||Math.abs(stack.peek() - s.charAt(i)) != 32){
                 stack.add(s.charAt(i));
             } else {
