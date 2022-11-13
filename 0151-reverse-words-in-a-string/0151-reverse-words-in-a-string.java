0904b292837d4a08a1e4f41afd282443237dc77c
sieun <sieun960513@naver.com>
2022-11-14 02:55:50 +0900

    Time: 7 ms (88.37%), Space: 43.8 MB (52.14%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -9,21 +9,5 @@ class Solution {
         }
         sb.deleteCharAt(sb.length()-1);
         return sb.toString();
-//         StringBuilder result = new StringBuilder();
-        
-//         StringBuilder temp = new StringBuilder();
-        
-//         for(int i = 0; i<s.length(); i++){
-//             char now = s.charAt(i);
-//             if(now == ' '){
-//                 if(temp.length() != 0){
-//                     result.append(temp);
-//                     temp = new StringBuilder();
-//                 }
-//             } else {
-//                 temp.append(now);
-//             }
-//         }
-//         return result.toString();
     }
 }
