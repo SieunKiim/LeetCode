c63d27cb6dbf4466e12661fcf28de6236e95d10f
sieun <sieun960513@naver.com>
2022-12-04 15:12:56 +0900

    Time: 69 ms (5.24%), Space: 74.8 MB (66.38%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,51 +1,33 @@
 class Solution {
     public int minimumAverageDifference(int[] nums) {
         int n = nums.length;
-        int ans = -1;
-        int minAvgDiff = Integer.MAX_VALUE;
-        long currPrefixSum = 0;
-        long totalSum = 0;
-        for (int index = 0; index < n; ++index) {
-            totalSum += nums[index];
-        }
-        for (int index = 0; index < n; ++index) {
-            currPrefixSum += nums[index];
-            long leftPartAverage = currPrefixSum;
-            leftPartAverage /= (index + 1);
-            long rightPartAverage = totalSum - currPrefixSum;
-            if (index != n - 1) {
-                rightPartAverage /= (n - index - 1);
-            }
-            int currDifference = (int) Math.abs(leftPartAverage - rightPartAverage);
-            if (currDifference < minAvgDiff) {
-                minAvgDiff = currDifference;
-                ans = index;
-            }
-        }
-        return ans;
-    }
-//     public int minimumAverageDifference(int[] nums) {
-//         int n = nums.length;
-//         int right = Arrays.stream(nums).sum();
-//         int left = 0;
-//         int compare = Integer.MAX_VALUE;
-//         int want = -1;
         
-//         for(int i = 0; i<n ;i++){
-//             left += nums[i];
-//             right -= nums[i];
+        // long right = Arrays.stream(nums).sum();
+        long right = Arrays.stream(nums).mapToLong(i->i).sum();
+        // long right = 0;
+        // for(int num : nums){
+        //     right += num;
+        // }
+        
+        long left = 0;
+        int compare = Integer.MAX_VALUE;
+        int want = -1;
+        
+        for(int i = 0; i<n ;i++){
+            left += nums[i];
+            right -= nums[i];
             
-//             int leftEv = left/(i+1);
-//             int rightEv = right;
-//             if(i != n-1){
-//                 rightEv /= (n-1-i);
-//             }
-//             int temp = Math.abs(leftEv - rightEv);
-//             if (compare > temp){
-//                 compare = temp;
-//                 want = i;
-//             }
-//         }
-//         return want;
-//     }
+            long leftEv = left/(i+1);
+            long rightEv = right;
+            if(i != n-1){
+                rightEv = rightEv/(n-1-i);
+            }
+            int temp = (int) Math.abs(leftEv - rightEv);
+            if (compare > temp){
+                compare = temp;
+                want = i;
+            }
+        }
+        return want;
+    }
 }
