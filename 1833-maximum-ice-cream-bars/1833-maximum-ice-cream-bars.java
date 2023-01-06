a344cf2d8c3bd3d52a8b9ebc38b427c6227252e7
sieun <sieun960513@naver.com>
2023-01-06 22:52:35 +0900

    Time: 120 ms (5.21%), Space: 95.6 MB (5.21%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int cost: costs){
+            pq.add(cost);
+        }
+        int count = 0;
+        while(!pq.isEmpty() && pq.peek() <= coins){
+            coins -= pq.poll();
+            count += 1;
+        }
+        return count;
+    }
+}
