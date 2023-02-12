435797bcc82398d9cebc0824c61ca3d15310fceb
김시은 <sieun960513@naver.com>
2023-02-12 14:26:16 +0900

    Time: 108 ms (40.39%), Space: 103.5 MB (56.09%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
@@ -2,20 +2,16 @@ class Solution {
     long fuel;
 
     public long dfs(int node, int parent, Map<Integer, List<Integer>> adj, int seats) {
-        // The node itself has one representative.
         int representatives = 1;
         if (!adj.containsKey(node)) {
             return representatives;
         }
         for (int child : adj.get(node)) {
             if (child != parent) {
-                // Add count of representatives in each child subtree to the parent subtree.
                 representatives += dfs(child, node, adj, seats);
             }
         }
         if (node != 0) {
-            // Count the fuel it takes to move to the parent node.
-            // Root node does not have any parent so we ignore it.
             fuel += Math.ceil((double) representatives / seats);
         }
         return representatives;
