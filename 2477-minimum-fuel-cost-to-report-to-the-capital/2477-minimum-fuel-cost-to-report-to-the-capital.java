f04154cf295b995ab280e7d6ee4fd743dcaf5a3a
김시은 <sieun960513@naver.com>
2023-02-12 14:26:03 +0900

    Time: 104 ms (44.55%), Space: 103.5 MB (56.09%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
@@ -1,43 +1,33 @@
 class Solution {
-    public long bfs(int n, Map<Integer, List<Integer>> adj, int[] degree, int seats) {
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 1; i < n; i++) {
-            if (degree[i] == 1) {
-                q.offer(i);
+    long fuel;
+
+    public long dfs(int node, int parent, Map<Integer, List<Integer>> adj, int seats) {
+        // The node itself has one representative.
+        int representatives = 1;
+        if (!adj.containsKey(node)) {
+            return representatives;
+        }
+        for (int child : adj.get(node)) {
+            if (child != parent) {
+                // Add count of representatives in each child subtree to the parent subtree.
+                representatives += dfs(child, node, adj, seats);
             }
         }
-
-        int[] representatives = new int[n];
-        Arrays.fill(representatives, 1);
-        long fuel = 0;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            fuel += Math.ceil((double) representatives[node] / seats);
-
-            for (int neighbor : adj.get(node)) {
-                degree[neighbor]--;
-                representatives[neighbor] += representatives[node];
-                if (degree[neighbor] == 1 && neighbor != 0) {
-                    q.offer(neighbor);
-                }
-            }
+        if (node != 0) {
+            // Count the fuel it takes to move to the parent node.
+            // Root node does not have any parent so we ignore it.
+            fuel += Math.ceil((double) representatives / seats);
         }
-        return fuel;
+        return representatives;
     }
 
     public long minimumFuelCost(int[][] roads, int seats) {
-        int n = roads.length + 1;
         Map<Integer, List<Integer>> adj = new HashMap<>();
-        int[] degree = new int[n];
-
         for (int[] road : roads) {
             adj.computeIfAbsent(road[0], k -> new ArrayList<Integer>()).add(road[1]);
             adj.computeIfAbsent(road[1], k -> new ArrayList<Integer>()).add(road[0]);
-            degree[road[0]]++;
-            degree[road[1]]++;
         }
-
-        return bfs(n, adj, degree, seats);
+        dfs(0, -1, adj, seats);
+        return fuel;
     }
 }
