2801bbd957fe61f2e24e55cb4d67899fce27e591
김시은 <sieun960513@naver.com>
2023-03-01 11:04:47 +0900

    Time: 53 ms (36.18%), Space: 52 MB (81.96%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,40 +1,26 @@
 class Solution {
-    private void swap(int[] arr, int index1, int index2) {
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
-    }
-    
-    private void heapify(int[] arr, int n, int i) {
-        int largest = i; 
-        int left = 2 * i + 1;
-        int right = 2 * i + 2; 
-        if (left < n && arr[left] > arr[largest]) {
-            largest = left;
+    private void countingSort(int[] arr) {
+        HashMap<Integer,Integer> counts = new HashMap<>();
+        int minVal = arr[0], maxVal = arr[0];
+        
+        for (int i = 0; i < arr.length; i++) {
+            minVal = Math.min(minVal, arr[i]);
+            maxVal = Math.max(maxVal, arr[i]);
+            counts.put(arr[i], counts.getOrDefault(arr[i], 0) + 1);
         }
-        if (right < n && arr[right] > arr[largest]) {
-            largest = right;
-        }
-        if (largest != i) {
-            swap(arr, i, largest); 
-            heapify(arr, n, largest);
-        }
-    }
-
-    private void heapSort(int[] arr) {
-        int n = arr.length;
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(arr, n, i);
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            swap(arr, 0, i);
-            heapify(arr, i, 0);
+        
+        int index = 0;
+        for (int val = minVal; val <= maxVal; ++val) {
+            while (counts.getOrDefault(val, 0) > 0) {
+                arr[index] = val;
+                index += 1;
+                counts.put(val, counts.get(val) - 1);
+            }
         }
     }
 
     public int[] sortArray(int[] nums) {
-        heapSort(nums);
+        countingSort(nums);
         return nums;
     }
 }
