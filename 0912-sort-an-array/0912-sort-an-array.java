5e3b8a0ac2834a5f02c22a5918e8c6fdd52c82df
김시은 <sieun960513@naver.com>
2023-03-01 11:04:02 +0900

    Time: 46 ms (40.25%), Space: 52.4 MB (64.24%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,6 +1,40 @@
 class Solution {
+    private void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    
+    private void heapify(int[] arr, int n, int i) {
+        int largest = i; 
+        int left = 2 * i + 1;
+        int right = 2 * i + 2; 
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            swap(arr, i, largest); 
+            heapify(arr, n, largest);
+        }
+    }
+
+    private void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, i, 0);
+        }
+    }
+
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        heapSort(nums);
         return nums;
     }
 }
