215912300dd2f89888eb2d08324fb26600c654cc
김시은 <sieun960513@naver.com>
2023-03-31 13:27:24 +0900

    Time: 7 ms (98.18%), Space: 40.9 MB (20.20%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.java
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.java
@@ -1,34 +1,24 @@
 class Solution {
     public int ways(String[] pizza, int k) {
-        int rows = pizza.length, cols = pizza[0].length();
-        int apples[][] = new int[rows + 1][cols + 1];
-        int dp[][][] = new int[k][rows][cols];
-        for (int row = rows - 1; row >= 0; row--) {
-            for (int col = cols - 1; col >= 0; col--) {
-                apples[row][col] = (pizza[row].charAt(col) == 'A' ? 1 : 0) + apples[row + 1][col] + apples[row][col + 1]
-                        - apples[row + 1][col + 1];
-                dp[0][row][col] = apples[row][col] > 0 ? 1 : 0;
-            }
-        }
-        int mod = 1000000007;
-        for (int remain = 1; remain < k; remain++) {
-            for (int row = 0; row < rows; row++) {
-                for (int col = 0; col < cols; col++) {
-                    for (int next_row = row + 1; next_row < rows; next_row++) {
-                        if (apples[row][col] - apples[next_row][col] > 0) {
-                            dp[remain][row][col] += dp[remain - 1][next_row][col];
-                            dp[remain][row][col] %= mod;
-                        }
-                    }
-                    for (int next_col = col + 1; next_col < cols; next_col++) {
-                        if (apples[row][col] - apples[row][next_col] > 0) {
-                            dp[remain][row][col] += dp[remain - 1][row][next_col];
-                            dp[remain][row][col] %= mod;
-                        }
-                    }
-                }
-            }
-        }
-        return dp[k - 1][0][0];
+        int m = pizza.length, n = pizza[0].length();
+        Integer[][][] dp = new Integer[k][m][n];
+        int[][] preSum = new int[m+1][n+1];
+        for (int r = m - 1; r >= 0; r--)
+            for (int c = n - 1; c >= 0; c--)
+                preSum[r][c] = preSum[r][c+1] + preSum[r+1][c] - preSum[r+1][c+1] + (pizza[r].charAt(c) == 'A' ? 1 : 0);
+        return dfs(m, n, k-1, 0, 0, dp, preSum);
+    }
+    int dfs(int m, int n, int k, int r, int c, Integer[][][] dp, int[][] preSum) {
+        if (preSum[r][c] == 0) return 0;
+        if (k == 0) return 1;
+        if (dp[k][r][c] != null) return dp[k][r][c];
+        int ans = 0;
+        for (int nr = r + 1; nr < m; nr++) 
+            if (preSum[r][c] - preSum[nr][c] > 0) 
+                ans = (ans + dfs(m, n, k - 1, nr, c, dp, preSum)) % 1_000_000_007;
+        for (int nc = c + 1; nc < n; nc++) 
+            if (preSum[r][c] - preSum[r][nc] > 0)
+                ans = (ans + dfs(m, n, k - 1, r, nc, dp, preSum)) % 1_000_000_007;
+        return dp[k][r][c] = ans;
     }
 }
