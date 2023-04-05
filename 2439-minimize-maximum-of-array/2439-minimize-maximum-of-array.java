5ac9be8345d465d27518a4e3f73523b78d8626a1
김시은 <sieun960513@naver.com>
2023-04-05 23:56:02 +0900

    Time: 17 ms (25.45%), Space: 62.3 MB (19.00%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -1,34 +1,52 @@
 class Solution {
     public int minimizeArrayValue(int[] nums) {
         // 그리디 풀이 O(n)
-        int n = nums.length;
-        long sum = 0;
-        int output = 0;
-        for(int i = 0; i< n ; i++){
-            sum += nums[i];
-            int mean = (int) Math.ceil((double)sum/(i+1));
-            if(mean <= nums[i]) output = Math.max(output, mean);
-        }
+//         int n = nums.length;
+//         long sum = 0;
+//         int output = 0;
+//         for(int i = 0; i< n ; i++){
+//             sum += nums[i];
+//             int mean = (int) Math.ceil((double)sum/(i+1));
+//             if(mean <= nums[i]) output = Math.max(output, mean);
+//         }
         
-        return output;
+//         return output;
         
 
         // 이분탐색 풀이
-//         int n = nums.length;
-//         int left = Integer.MAX_VALUE;
-//         int right = Integer.MIN_VALUE;
-//         for(int num : nums){
-//             left = Math.min(left, num);
-//             right = Math.max(right, num);
-//         }
-        
-        
-//         while(left < right){
-//             int mid = (left+right) / 2;
-            
-            
-//         }
-        
-//         return left;
+//         // initialize left and right pointers for binary search
+        int left = 0, right = Arrays.stream(nums).max().getAsInt();
+        // perform binary search to find the minimum possible value of the maximum integer of nums after performing any number of operations
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (check(nums, mid))
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        // return the minimum possible value of the maximum integer of nums after performing any number of operations
+        return left;
+    }
+    public boolean check(int[] nums, int k) {
+        // variable to keep track of how many moves we have
+        long have = 0;
+        // iterate over the array
+        for (int n : nums) {
+            // if the number is less than or equal to k, we don't need to do anything
+            if (n <= k) {
+                // add the number of moves needed to make it k to the total number of moves we have
+                have += k - n;
+            } else {
+                // if the number is greater than k, we need to move some of its value to the previous element
+                // check if we have enough moves left to make this move
+                if (have < n - k)
+                    return false;
+                else
+                    // if we have enough moves, subtract the number of moves needed to make this move from the total number of moves we have
+                    have -= (n - k);
+            }
+        }
+        // if we reach here, it means we were able to achieve a maximum value of k or less using the given number of moves
+        return true;
     }
 }
