1f180bdd643937634b64300d495d2408daea7e2f
김시은 <sieun960513@naver.com>
2023-04-18 13:36:57 +0900

    Time: 0 ms (100.00%), Space: 40.3 MB (98.62%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -5,21 +5,14 @@ class Solution {
         int word1Len = word1.length();
         int word2Len = word2.length();
         StringBuilder sb = new StringBuilder();
-        while(a < word1Len && b < word2Len){
-            sb.append(word1.charAt(a));
-            sb.append(word2.charAt(b));
-            a+=1;
-            b+=1;
-        }
-        if(a == word1Len){
-            while(b<word2Len){
-                sb.append(word2.charAt(b));
-                b+= 1;
-            }
-        } else{
-            while(a<word1Len){
+        while(a < word1Len || b < word2Len){
+            if(a < word1Len){
                 sb.append(word1.charAt(a));
                 a+=1;
+            }
+            if(b<word2Len){
+                sb.append(word2.charAt(b));
+                b+=1;
             }
         }
         return sb.toString();
