92e35f2be2fbb68c1aa7f4f3eef3efb4a790d472
김시은 <sieun960513@naver.com>
2023-05-10 00:04:15 +0900

    Time: 0 ms (100.00%) | Memory: 40.6 MB (58.06%) - LeetSync

--- spiral-matrix/spiral-matrix.java
+++ spiral-matrix/spiral-matrix.java
@@ -1,19 +1,14 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         int[][] direction = {{0,1},{1,0},{0,-1},{-1,0}};
-
         int n = matrix.length;
         int m = matrix[0].length;
-
         boolean[][] visited = new boolean[n][m];
-
         int d = 0;
-
         int i = 0;
         int j = 0;
-        int stuck = 0;
         ArrayList<Integer> output = new ArrayList<>();
-        while(stuck < 4){
+        while(true){
              if (i >= n || i < 0 || j >= m || j < 0 ||visited[i][j]) break;
             visited[i][j] = true;
             output.add(matrix[i][j]);
@@ -22,7 +17,6 @@ class Solution {
             if(ni >= n || ni<0 || nj >= m || nj <0 || visited[ni][nj]){
                 d = (d+1)%4;
             }
-
             i = i + direction[d][0];
             j = j + direction[d][1];
         }
