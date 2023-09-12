e79d00f2c380c2e41c04306d75048023d08de363
김시은 <sieun960513@naver.com>
2023-09-12 23:54:28 +0900

    Time: 7 ms (98.85%) | Memory: 44.7 MB (37.10%) - LeetSync

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int minDeletions(String s) {
+        int[] freq = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++; 
+        }
+        Arrays.sort(freq);
+        int del = 0; 
+        for (int i = 24; i >= 0; i--) {
+            if (freq[i] == 0) {
+                break; 
+            }
+            if (freq[i] >= freq[i + 1]) {
+                int prev = freq[i];
+                freq[i] = Math.max(0, freq[i + 1] - 1);
+                del += prev - freq[i]; 
+            }
+        }
+        return del;
+    }
+}
