a954e2eb5cfb103900c4a6623e63200f094eee89
김시은 <sieun960513@naver.com>
2023-11-02 22:22:01 +0900

    Time: 1 ms (48.09%) | Memory: 43.2 MB (36.07%) - LeetSync

---
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -0,0 +1,23 @@
+class Solution {
+    int count = 0;
+    
+    Pair<Integer, Integer> postOrder(TreeNode root) {
+        if (root == null) {
+            return new Pair(0, 0);
+        }
+        Pair<Integer, Integer> left = postOrder(root.left);
+        Pair<Integer, Integer> right = postOrder(root.right);
+        
+        int nodeSum = left.getKey() + right.getKey() + root.val;
+        int nodeCount = left.getValue() + right.getValue() + 1;
+        if (root.val == nodeSum / (nodeCount)) {
+            count++;
+        }
+        return new Pair(nodeSum, nodeCount);
+    }
+    
+    public int averageOfSubtree(TreeNode root) {
+        postOrder(root);
+        return count;
+    }
+}
