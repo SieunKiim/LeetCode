abfb8d758687be5653a2591adc738fc783fd49f0
김시은 <sieun960513@naver.com>
2023-12-05 01:08:21 +0900

    Time: 1 ms (91.57%) | Memory: 41.2 MB (52.85%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -1,7 +1,6 @@
 class Solution {
     private List<String> sameDigitNumbers = List.of("999", "888", "777", "666", "555", "444", "333", "222", "111", "000");
 
-    // Check whether the 'num' string contains the 'sameDigitNumber' string or not.
     private boolean contains(String sameDigitNumber, String num) {
         for (int index = 0; index <= num.length() - 3; ++index) {
             if (num.charAt(index) == sameDigitNumber.charAt(0) &&
@@ -14,14 +13,11 @@ class Solution {
     }
 
     public String largestGoodInteger(String num) {
-        // Iterate on all 'sameDigitNumbers' and check if the string 'num' contains it.
         for (String sameDigitNumber : sameDigitNumbers) {
             if (contains(sameDigitNumber, num)) {
-                // Return the current 'sameDigitNumbers'.
                 return sameDigitNumber;
             }
         }
-        // No 3 consecutive same digits are present in the string 'num'.
         return "";
     }
 }
