7c7760571518af1a8b84d03e612fc585e4dc6836
김시은 <sieun960513@naver.com>
2025-08-22 00:18:01 +0900

    Time: 8 ms (43.15%) | Memory: 45.5 MB (63.36%) - LeetSync

---
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int numSubmat(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int res = 0;
+        int[][] row = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    row[i][j] = mat[i][j];
+                } else {
+                    row[i][j] = mat[i][j] == 0 ? 0 : row[i][j - 1] + 1;
+                }
+                int cur = row[i][j];
+                for (int k = i; k >= 0; k--) {
+                    cur = Math.min(cur, row[k][j]);
+                    if (cur == 0) {
+                        break;
+                    }
+                    res += cur;
+                }
+            }
+        }
+        return res;
+    }
+}
