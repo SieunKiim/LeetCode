c5e48b310ea4493c99d975f04bda61ccff9117ba
김시은 <sieun960513@naver.com>
2025-09-11 00:11:20 +0900

    Time: 118 ms (20.69%) | Memory: 63.3 MB (15.52%) - LeetSync

--- 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.java
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.java
@@ -1,32 +1,40 @@
 class Solution {
-
     public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
-        Set<Integer> cncon = new HashSet<>();
-        for (int[] friendship : friendships) {
-            Map<Integer, Integer> mp = new HashMap<>();
-            boolean conm = false;
-            for (int lan : languages[friendship[0] - 1]) {
-                mp.put(lan, 1);
+        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
+        for (int i = 0; i < languages.length; i++) {
+            HashSet<Integer> temp = new HashSet<>();
+            for (int lang : languages[i]) {
+                temp.add(lang);
             }
-            for (int lan : languages[friendship[1] - 1]) {
-                if (mp.containsKey(lan)) {
-                    conm = true;
-                    break;
-                }
-            }
-            if (!conm) {
-                cncon.add(friendship[0] - 1);
-                cncon.add(friendship[1] - 1);
+            map.put(i + 1, temp);
+        }
+
+        Set<Integer> unreachableUsers = new HashSet<>();
+        for (int[] f : friendships) {
+            int a = f[0], b = f[1];
+            HashSet<Integer> common = new HashSet<>(map.get(a));
+            common.retainAll(map.get(b));
+            if (common.isEmpty()) { // 대화 불가
+                unreachableUsers.add(a);
+                unreachableUsers.add(b);
             }
         }
-        int max_cnt = 0;
-        int[] cnt = new int[n + 1];
-        for (int friendship : cncon) {
-            for (int lan : languages[friendship]) {
-                cnt[lan]++;
-                max_cnt = Math.max(max_cnt, cnt[lan]);
+
+        if (unreachableUsers.isEmpty()) return 0;
+
+        int[] langCount = new int[n + 1];
+        for (int user : unreachableUsers) {
+            for (int lang : map.get(user)) {
+                langCount[lang]++;
             }
         }
-        return cncon.size() - max_cnt;
+
+        int minTeach = Integer.MAX_VALUE;
+        for (int lang = 1; lang <= n; lang++) {
+            int teachCount = unreachableUsers.size() - langCount[lang];
+            minTeach = Math.min(minTeach, teachCount);
+        }
+
+        return minTeach;
     }
 }
