ffcf3b1d9e0793639de7bcf9bf78bb5fdde5e0f5
김시은 <sieun960513@naver.com>
2025-09-17 21:38:42 +0900

    Time: 1153 ms (5.15%) | Memory: 73.4 MB (75.15%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.java
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.java
@@ -1,61 +1,59 @@
 class FoodRatings {
 
-     HashMap<String, PriorityQueue<Food>> cuisineBestFoods;
-        HashMap<String, Food> foodMap;
-
-
-        public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
-            cuisineBestFoods = new HashMap<>();
-            foodMap = new HashMap<>();
-            int n = foods.length;
-            for (int i = 0; i < n; i++) {
-                Food food = new Food(foods[i], cuisines[i], ratings[i]);
-                putFood(food);
-            }
+    public class Food{
+        String name;
+        int rate;
+        public Food(String name, int rate){
+            this.name = name;
+            this.rate = rate;
         }
 
-        public void changeRating(String food, int newRating) {
-            Food food1 = foodMap.get(food);
-            putFood(food1.changeRate(newRating));
+        @Override
+        public String toString(){
+            return "(" + name + ", " + rate + ")";
         }
+    }
 
-        public String highestRated(String cuisine) {
-            return cuisineBestFoods.get(cuisine).peek().name;
-        }
+    public HashMap<String, PriorityQueue<Food>> map= new HashMap<>();
+    public HashMap<String, String> cuisineDict = new HashMap<>();
+    public HashMap<String, Food> foodDict = new HashMap<>();
 
-        private void putFood(Food food){
-            foodMap.put(food.name, food);
-            if (!cuisineBestFoods.containsKey(food.cuisine)) {
-                cuisineBestFoods.put(food.cuisine, new PriorityQueue<>());
-            }
-            PriorityQueue<Food> foods = cuisineBestFoods.get(food.cuisine);
-            foods.remove(food);
-            foods.add(food);
-        }
-
-        class Food  implements Comparable<Food> {
-            String name;
-            String cuisine;
-            int rate;
-
-            public Food(String name, String cuisine, int rate) {
-                this.name = name;
-                this.cuisine = cuisine;
-                this.rate = rate;
-            }
-
-            public Food changeRate(int num) {
-                this.rate = num;
-                return this;
-            }
-            @Override
-            public int compareTo(Food o2) {
-                if (this.rate == o2.rate) {
-                    return this.name.compareTo(o2.name);
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+        
+        for(int i =0; i<foods.length;i++){
+            String food = foods[i];
+            String cuisine = cuisines[i];
+            int rate = ratings[i];
+            PriorityQueue<Food> temp = map.getOrDefault(cuisine, new PriorityQueue<>((o1,o2) -> {
+                if(o1.rate == o2.rate){
+                    return o1.name.compareTo(o2.name);
                 }
-                return o2.rate - this.rate;
-            }
+                return o2.rate-o1.rate;}
+                ));
+            Food f = new Food(food,rate);
+            temp.add(f);
+            map.put(cuisine, temp);
+
+            cuisineDict.put(food, cuisine);
+            foodDict.put(food, f);
         }
+        
+    }
+    
+    public void changeRating(String food, int newRating) {
+
+        var cui = cuisineDict.get(food);
+        PriorityQueue<Food> pq = map.get(cui);
+        
+        pq.remove(foodDict.get(food));
+        Food nf = new Food(food, newRating);
+        pq.add(nf);
+        foodDict.put(food, nf);
+    }
+    
+    public String highestRated(String cuisine) {
+        return map.get(cuisine).peek().name;
+    }
 }
 
 /**
