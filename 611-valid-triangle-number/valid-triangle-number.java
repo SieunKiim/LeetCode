eec630d9f95daca9bf680ae582adf8ed61a9bba6
김시은 <sieun960513@naver.com>
2025-09-27 00:01:58 +0900

    Time: 223 ms (11.11%) | Memory: 43.2 MB (92.68%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.java
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -1,24 +1,33 @@
-public class Solution {
-    int binarySearch(int nums[], int l, int r, int x) {
-        while (r >= l && r < nums.length) {
-            int mid = (l + r) / 2;
-            if (nums[mid] >= x)
-                r = mid - 1;
-            else
-                l = mid + 1;
-        }
-        return l;
-    }
+class Solution {
     public int triangleNumber(int[] nums) {
-        int count = 0;
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            int k = i + 2;
-            for (int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
-                k = binarySearch(nums, 0, nums.length - 1, nums[i] + nums[j]);
-                count += k - j - 1;
+        int count = 0;
+        // HashSet<Tri> set = new HashSet<>();
+        for(int i = 0; i< nums.length;i++){
+            for(int j = i+1; j< nums.length; j++){
+                int temp = nums[i] + nums[j];
+                int index = getIndex(nums, temp)-1;
+                // System.out.println("i : "+i+ " , j : " + j);
+                // System.out.println("indeix : "+index);
+                if(index > j){
+                    count +=(index-j);
+                }
             }
         }
+        // System.out.println(set);
         return count;
+    }
+
+    public int getIndex(int[] arr, int x){
+        int left = 0, right = arr.length; // right는 arr.length까지 가능
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < x) {
+                left = mid + 1; // x보다 작은 값은 버림
+            } else {
+                right = mid;    // x 이상일 때는 mid 포함 가능
+            }
+        }
+        return left; // 또는 right (둘이 같음)
     }
 }
