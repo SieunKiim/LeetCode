7cb3b5adc0a528d41ef819ee433d9d2d67237490
김시은 <sieun960513@naver.com>
2025-09-27 00:00:09 +0900

    Time: 217 ms (11.21%) | Memory: 42.9 MB (99.05%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.java
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -15,7 +15,7 @@ public class Solution {
         for (int i = 0; i < nums.length - 2; i++) {
             int k = i + 2;
             for (int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
-                k = binarySearch(nums, k, nums.length - 1, nums[i] + nums[j]);
+                k = binarySearch(nums, 0, nums.length - 1, nums[i] + nums[j]);
                 count += k - j - 1;
             }
         }
