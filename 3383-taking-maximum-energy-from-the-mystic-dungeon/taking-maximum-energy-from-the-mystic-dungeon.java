cbd95582e81c61746a4b7d0ef0f3ecc3bb989006
김시은 <sieun960513@naver.com>
2025-10-10 22:13:48 +0900

    Time: 3 ms (60.69%) | Memory: 61.1 MB (62.07%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i = 1;i < n+1 ;i++){
             dp[i] = Math.max(dp[Math.max(0,i-k)] + energy[i-1], energy[i-1]);
         }
-        System.out.println(Arrays.toString(dp));
+        // System.out.println(Arrays.toString(dp));
         for(int i = n; i>n-k;i--){
             output = Math.max(output,dp[i]);
         }
