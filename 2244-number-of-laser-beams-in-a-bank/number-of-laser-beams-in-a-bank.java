e07b24700e669de008d9c5fa7349fcbcd6c0de5c
김시은 <sieun960513@naver.com>
2025-10-27 22:50:45 +0900

    Time: 10 ms (89.60%) | Memory: 45.4 MB (35.36%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -1,25 +1,28 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int lastVal = 0;
-        int output = 0;
+        int n = bank.length;
+        int m = bank[0].length();
+        int last = 0;
+        int sum = 0;
         for(String s : bank){
-            int now = getDeviceCount(s);
-            if(now != 0){
-                output += lastVal * now;
-                lastVal = now;
+            char[] arr = s.toCharArray();
+            int temp = countDevice(arr);
+            if(temp == 0) continue;
+            if(last == 0){
+                last = temp;
+            } else {
+                sum += last * temp;
+                last = temp;
             }
         }
-        return output;
+        return sum;
     }
 
-    public int getDeviceCount(String s){
-        int count = 0;
-        for(int i = 0; i < s.length();i++){
-            char now = s.charAt(i);
-            if(now == '1'){
-                count += 1;
-            }
+    private int countDevice(char[] arr){
+        int output = 0;
+        for(char a : arr){
+            if(a == '1')output += 1;
         }
-        return count;
+        return output;
     }
 }
