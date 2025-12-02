e2da6058e04ac370823ae8a8618af3b8d07c6610
김시은 <sieun960513@naver.com>
2025-12-02 21:30:21 +0900

    Time: 38 ms (73.47%) | Memory: 207.3 MB (9.18%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
@@ -1,18 +1,18 @@
 class Solution {
 
     public int countTrapezoids(int[][] points) {
-        Map<Integer, Integer> pointNum = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         final int mod = 1000000007;
         long ans = 0;
         long sum = 0;
-        for (int[] point : points) {
-            pointNum.put(point[1], pointNum.getOrDefault(point[1], 0) + 1);
+        for(int[] point :points){
+            map.put(point[1], map.getOrDefault(point[1], 0)+1);
         }
-        for (int pNum : pointNum.values()) {
-            long edge = ((long) pNum * (pNum - 1)) / 2;
-            ans = (ans + edge * sum) % mod;
-            sum = (sum + edge) % mod;
+        for(int num : map.values()){
+            int temp  = (int)(((long) num * (num-1) / 2 )%mod);
+            ans =  (ans + (sum * temp) % mod) % mod;
+            sum = (sum + temp) % mod; 
         }
-        return (int) ans;
+        return (int) ans%mod;
     }
 }
