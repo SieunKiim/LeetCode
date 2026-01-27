02e5298dd280c2778a8997fee8518b3ef8c5947c
김시은 <sieun960513@naver.com>
2026-01-27 23:14:24 +0900

    Time: 76 ms (83.81%) | Memory: 261.6 MB (69.63%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -1,44 +1,43 @@
 class Solution {
     public int minCost(int n, int[][] edges) {
-        // int[][] map = new int[n][n];
-        HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
-        int[] output = new int[n];
+        List<List<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
         
-        Arrays.fill(output, Integer.MAX_VALUE);
-
-        for(int[] edge: edges){
+        for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
             int w = edge[2];
-
-            HashMap<Integer, Integer> row1 = map.getOrDefault(u, new HashMap<>());
-            Integer weight1 = row1.getOrDefault(v, Integer.MAX_VALUE);
-            row1.put(v, Math.min(weight1, w));
-            map.put(u, row1);
-
-            HashMap<Integer, Integer> row2 = map.getOrDefault(v, new HashMap<>());
-            Integer weight2 = row2.getOrDefault(u, Integer.MAX_VALUE);
-            row2.put(u, Math.min(weight2, w * 2));
-            map.put(v, row2);
-
+            
+            graph.get(u).add(new int[]{v, w});
+            graph.get(v).add(new int[]{u, w * 2});
         }
-
-        PriorityQueue<int[]> que = new PriorityQueue<>((o1,o2)->o1[1]-o2[1]);
-        que.add(new int[]{0,0});
-        output[0] = 0;
-        while(!que.isEmpty()){
-            int[] now = que.poll();
-            if (now[0] == n-1) return now[1];
-            HashMap<Integer, Integer> row = map.getOrDefault(now[0], null);
-            if(row == null) continue;
-            for(int next : row.keySet()){
-                int tempWeight = row.get(next);
-                if(tempWeight + now[1] < output[next]){
-                    output[next] = tempWeight + output[now[0]];
-                    que.add(new int[]{next, output[next]});
+        
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        pq.offer(new int[]{0, 0}); // {노드, 거리}
+        
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int node = curr[0];
+            int cost = curr[1];
+            if (cost > dist[node]) continue;
+            if (node == n - 1) return cost;
+            for (int[] next : graph.get(node)) {
+                int nextNode = next[0];
+                int nextCost = cost + next[1];
+                
+                if (nextCost < dist[nextNode]) {
+                    dist[nextNode] = nextCost;
+                    pq.offer(new int[]{nextNode, nextCost});
                 }
             }
         }
-        return output[n-1] == Integer.MAX_VALUE ? -1 : output[n-1] ;
+        
+        return dist[n - 1] == Integer.MAX_VALUE ? -1 : dist[n - 1];
     }
 }
