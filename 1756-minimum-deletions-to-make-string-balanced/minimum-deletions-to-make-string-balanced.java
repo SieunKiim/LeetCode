8fe259ad423ee8c7024232da3e744a9419adcb25
김시은 <sieun960513@naver.com>
2026-02-07 14:22:32 +0900

    Time: 19 ms (93.09%) | Memory: 47.8 MB (45.39%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,23 +1,15 @@
 class Solution {
     public int minimumDeletions(String s) {
-        // 앞에있는 b를 빼거나, 뒤에있는 a를 빼거나
-        int n = s.length();
-        int[][] preSum = new int[n+1][2];
-        for(int i =1 ;i<=n;i++){
-            preSum[i][0] = preSum[i-1][0];
-            preSum[i][1] = preSum[i-1][1];
-            preSum[i][s.charAt(i-1)-'a'] += 1;
+        int bCount = 0;  
+        int deletions = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == 'b') {
+                bCount++;
+            } else {
+                deletions = Math.min(deletions + 1, bCount);
+            }
         }
-        // for(int[] arr : preSum){
-        //     System.out.println(Arrays.toString(arr));
-        // }
-        
-        int output = Integer.MAX_VALUE;
-        for(int i =1;i<=n;i++){
-            int countA = preSum[n][0] - preSum[i][0];
-            int countB = preSum[i-1][1];
-            output = Math.min(output, countA+countB);
-        }
-        return output;
+        return deletions;
     }
 }
