927e8cc29b8c5ad353bf102f118d70f24a08a602
김시은 <sieun960513@naver.com>
2026-04-25 19:34:29 +0900

    Time: 0 ms (100.00%) | Memory: 43.9 MB (47.72%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,27 +1,42 @@
 class Solution {
     public int search(int[] nums, int target) {
+
+        // 1. 로테이션 갯수 구하기
         int n = nums.length;
-        int left = 0, right = n - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] >= nums[left]) {
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else {
-                if (target <= nums[right] && target > nums[mid]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+        int left = 0;
+        int right = nums.length-1;
+        int rotatedIndex = n;
+        for(int i =1;i<nums.length;i++){
+            if(nums[i] < nums[i-1]) {
+                rotatedIndex = i;
+                break;
             }
         }
-        return -1;
+
+        // 2. target 넘버 인덱스 구하기
+        if(target >= nums[0]){
+            left = 0;
+            right = rotatedIndex-1;
+        } else {
+            left = rotatedIndex;
+            right = n-1;
+        }
+
+        if (left > right) return -1;
+
+        while(left < right){
+            int mid = (left + right)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[mid] < target){
+                left = mid+1;
+            } else {
+                right = mid;
+            }
+        }
+        if(nums[left] != target)return -1;
+        return left;
     }
 }
