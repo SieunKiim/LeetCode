495eb2bc59126198df9f8cb1b7070ba9f72ae837
김시은 <sieun960513@naver.com>
2026-04-25 22:44:22 +0900

    Time: 5 ms (27.44%) | Memory: 52.4 MB (35.36%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,45 +1,41 @@
 class Solution {
-
-    public int[] dx = {-1, 0, 1, 0};
-    public int[] dy = {0, -1, 0, 1};
-
-
-    public boolean[][] visited;
-    public char[][] map;
-    public int n;
-    public int m;
-
-
+    char[][] map;
+    int[][] D = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    int n;
+    int m;
+    boolean[][] visited;
     public int numIslands(char[][] grid) {
-        int islandCount = 0;
+        map = grid;
         n = grid.length;
         m = grid[0].length;
-        map = grid;
-
+        int count = 0;
         visited = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (map[i][j] == '1' && !visited[i][j]) {
-                    dfs(i, j);
-                    islandCount += 1;
-                }
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j] == '1' && !visited[i][j]) {
+                    dfs(i,j);
+                    count += 1;
+                            // System.out.println("count  :: i : " + i + " j : " + j);
+                };
+                visited[i][j] = true;
             }
         }
-        return islandCount;
+        return count;
     }
 
-    public void dfs(int x, int y) {
-        visited[x][y] = true;
-
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (0 <= nx && nx < n && 0 <= ny && ny < m && map[nx][ny] == '1' && !visited[nx][ny]) {
-                dfs(nx, ny);
+    private void dfs(int i , int j){
+        // System.out.println("i : " + i + " j : " + j);
+        visited[i][j] =true;
+         for(int d=0; d<4;d++){
+            int ni = D[d][0] + i;
+            int nj = D[d][1] + j;
+            if(canGo(ni, nj) && !visited[ni][nj] && map[ni][nj] == '1'){
+                dfs(ni,nj);
             }
         }
     }
 
-
+    private boolean canGo(int i, int j){
+        return 0<= i  && i< n && 0<= j && j< m;
+    }
 }
